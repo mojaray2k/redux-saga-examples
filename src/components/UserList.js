c4499afae76946427928ac336a83eb19e670f7fe
Amen Moja Ra <mojaray2k@gmail.com>
2021-04-28 00:38:47 -0400

    created user list initial ui with delete button

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,17 +1,40 @@
-import {ListGroup, ListGroupItem} from 'reactstrap';
+import { Button, ListGroup, ListGroupItem } from 'reactstrap';
 
 import React from 'react';
 
-const UserList = ({users}) => {
-    return (
-        <ListGroup>
-            {users.map((user) => (
-                <ListGroupItem key={user.id}>
-                    {user.firstName}
-                </ListGroupItem>
-            ))}
-        </ListGroup>
-    )
-}
+const UserList = ({ users }) => {
+  return (
+    <ListGroup>
+      {users
+        .sort((a, b) => {
+          if (a.firstName > b.firstName) {
+            return 1;
+          } else if (a.firstName < b.firstName) {
+            return -1;
+          } else if (a.lastName > b.lastName) {
+            return 1;
+          } else if (a.lastName < b.lastName) {
+            return -1;
+          } else {
+            return 0;
+          }
+        })
+        .map((user) => (
+          <ListGroupItem key={user.id}>
+            <section style={{ display: 'flex', margin: "auto 0" }}>
+              <div style={{ flexGrow: 1 }}>
+                {user.firstName} {user.lastName}
+              </div>
+              <div>
+                <Button size="sm" color="danger" outline>
+                    Delete
+                </Button>
+              </div>
+            </section>
+          </ListGroupItem>
+        ))}
+    </ListGroup>
+  );
+};
 
 export default UserList;
