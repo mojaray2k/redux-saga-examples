4fd9b85ada1436d079a1d78350448580b51cd72c
Amen Moja Ra <mojaray2k@gmail.com>
2021-05-01 02:54:02 -0400

    added new user form

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 
+import NewUserForm from './NewUserForm';
 import UserList from './UserList';
 import { connect } from 'react-redux';
-import {getUsersRequest} from '../actions/users'
+import {getUsersRequest} from '../actions/users';
 
 class App extends Component {
   constructor(props) {
@@ -10,11 +11,15 @@ class App extends Component {
     this.props.getUsersRequest();
   }
 
+  handleSubmit = ({firstName, lastName}) => {
+    console.log(firstName, lastName)
+  }
 
   render() {
     const users = this.props.users;
     return (
       <div style={{margin: "0 auto", padding: "20px", maxWidth: "600px"}}>
+        <NewUserForm onSubmit={this.handleSubmit} />
         <UserList users={users.items}/>
       </div>
     );
